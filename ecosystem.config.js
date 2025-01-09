1df2fb05f9bded1ce9f3d3293c51f4efc22c1720
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2025-01-09 14:57:58 +0700

    #fix: thuanvv fix config pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'minimal_english_be_new',
-      script: './dist/web/app.js',
+      script: './dist/web/server.js',
       instances: 'max',
       exec_mode: 'cluster',
       env: {
