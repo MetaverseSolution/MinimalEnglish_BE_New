1b15ef10a24b5ea12e659caf676410db1ce276db
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2025-01-11 21:01:35 +0700

    #fix: thuanvv fix api document

--- src/web/routes/documents.ts
+++ src/web/routes/documents.ts
@@ -16,9 +16,11 @@ export default async function documentRoutes(fastify: FastifyRouteInstance) {
         type: 'object',
         properties: {
           language: { type: 'string', description: 'Language code (e.g., vi, en, etc.)' },
-          document_type_id: { type: 'string', description: 'ID of pages database' }
+          document_type_id: { type: 'string', description: 'ID of pages database' },
+          size: { type: 'number', description: 'Size number of records (e.g., 10.)' },
+          page: { type: 'number', description: 'Page number of database (e.g., 1.)' }
         },
-        required: ['language', 'document_type_id'],
+        required: ['language', 'document_type_id', 'size', 'page'],
       },
       response: {
         200: {
